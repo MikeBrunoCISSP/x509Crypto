eaf740c319afbb9c1ada332e6fc7f2fac1d7345b
Mike Bruno <mikebrunocissp@gmail.com>
2023-07-02 22:27:14 -0400

    Added another test

---
+++ Tests/X509ContextTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Org.X509Crypto;
+
+namespace Tests {
+    [TestClass]
+    public class X509ContextTests {
+        [TestMethod]
+        public void TestEquals() {
+            var userReadOnly = X509Context.UserReadOnly;
+            var other = X509Context.UserReadOnly;
+            var userFull = X509Context.UserFull;
+            Assert.AreEqual(userReadOnly, other);
+            Assert.AreNotEqual(userReadOnly, userFull);
+        }
+    }
+}
